d8f627beedc935aeaf50b988ed97ba327f932389
Sagar Patil <sagarreddypatil@gmail.com>
2020-08-14 21:19:37 -0400

    Fix everything?

---
+++ Assets/Scripts/PhysicSliderController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum PhysicControlType{
+    StaticFriction,
+    DynamicFriction,
+    Elasticity
+}
+
+public class PhysicSliderController : MonoBehaviour
+{
+    public TextMeshPro sliderValue;
+    public Slider slider;
+    public string type;
+
+    public GameObject controlledObject { set; private get; }
+
+    void Start()
+    {
+        sliderValue = GetComponent<TextMeshPro>();
+    }
+
+    public void UpdateValue()
+    {
+        sliderValue.text = Mathf.Round(slider.value * 100).ToString();
+    }
+}
---
+++ Assets/Scripts/SimulationManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimulationManager : MonoBehaviour
+{
+    public Slider timeSlider;
+    
+    void SetTime(float newTime) {
+        Time.timeScale = newTime;
+        Time.fixedDeltaTime = newTime * .02f;
+        timeSlider.value = newTime;
+    }
+    
+    void Start()
+    {
+        SetTime(0);
+    }
+
+    public void UpdateTime() {
+        SetTime(timeSlider.value);
+    }
+
+    public void Pause()
+    {
+        SetTime(0);
+    }
+
+    public void Resume()
+    {
+        SetTime(1);
+    }
+}
