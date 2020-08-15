f918d6019a42237068802ebaaa6fedc8ba91f877
shricharanks108 <62522322+shricharanks108@users.noreply.github.com>
2020-08-15 14:47:21 -0400

    Create Main Menu

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public void GoToInclinePlaneSim()
+    {
+        SceneManager.LoadScene("InclinedPlaneSim");
+    }
+
+    public void EndApplication()
+    {
+        Application.Quit();
+    }
+}
