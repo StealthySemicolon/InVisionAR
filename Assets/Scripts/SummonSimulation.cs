5c2ee61b7e745d47998f8a3db779f730e69f093e
shricharanks108 <62522322+shricharanks108@users.noreply.github.com>
2020-08-15 10:22:55 -0400

    Finish AR Plane

---
+++ Assets/Scripts/SummonSimulation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonSimulation : MonoBehaviour
+{
+    public GameObject simulationToSpawn;
+    private PlacementIndicatorScript placementIndicator;
+
+    void Start()
+    {
+        placementIndicator = FindObjectOfType<PlacementIndicatorScript>();
+    }
+
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject simulation = Instantiate(simulationToSpawn, placementIndicator.transform.position + new Vector3(0, 5, 0), placementIndicator.transform.rotation);
+        }
+    }
+
+}
